e3f8e78790f85a2d50be5dfdc16a3b29701ae09d
Aras14HD <jorimbk@gmail.com>
2023-01-26 20:24:38 +0100

    -add test script

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
+echo "  Testing Rust (9 9)"
+cargo build -r | /dev/null
+./target/release/ComplexCombinations 9 9
+echo "  With 9 9 9 9 9"
+./target/release/ComplexCombinations 9 9 9 9 9
 
-echo "Testing Rust"
-cargo build -r > null
-./target/release/ComplexCombinations
+echo "  Testing Java (9 9)"
+java Solver.java 9 9
 
-echo "Testing Java"
-javac Solver.java
+echo "  Testing JavaScript (9 9)"
+node Solver.js 9 9
+echo "  With 9 9 9 9 9"
+node Solver.js 9 9 9 9 9
 
-
-echo "Testing"
+echo "      Testing Python (9 9)"
+python Solver.py 9 9
+echo "  With 9 9 9 9 9"
+python Solver.py 9 9 9 9 9
